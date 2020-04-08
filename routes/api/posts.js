e3468f51f177b31fef3edced15592face6bcf522
dinoxas <dhassiotis07@gmail.com>
2020-04-08 19:43:19 +0100

    created route for adding a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/posts
-// @desc Test route
-// @acess Public (no teken needed)
-router.get('/', (req, res) => res.send('Posts route'));
+// @route POST api/posts
+// @desc Create a post
+// @acess Private
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // req.user coming from middleware. Can be accessed from anywhere in a protected route
+      // -password will leave off the password from data
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
