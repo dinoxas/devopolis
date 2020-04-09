b7841186ba87e9411685627dc4764524f3751620
dinoxas <dhassiotis07@gmail.com>
2020-04-09 20:15:31 +0100

    created redux store, alert action and reducer and Alert component

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Alert = ({ alerts }) =>
+  alerts !== null &&
+  alerts.length > 0 &&
+  alerts.map((alert) => (
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      {alert.msg}
+    </div>
+  ));
+
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  // key can be caalled anything, get state from alert reducer
+  alerts: state.alert
+});
+
+export default connect(mapStateToProps)(Alert);
