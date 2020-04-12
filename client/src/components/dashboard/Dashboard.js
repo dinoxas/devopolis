2e61da81c3e3a797bf1be5fdfd335ed6f5fa9ace
dinoxas <dhassiotis07@gmail.com>
2020-04-12 20:48:53 +0100

    created Private Route for dashboard component

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+// import { login } from '../../actions/auth';
+
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
